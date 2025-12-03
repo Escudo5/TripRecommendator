d0143da6a67650acfef9816c6727bd1554f768a4
Escudo5 <sergiomargar03@gmail.com>
2025-12-03 16:27:28 +0100

    creo archivos y Docker funciona bien, puerto 3000 funciona

---
+++ ex00/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>¡Hola desde Docker!</h1>
+      <p>Si ves esto, tu entorno funciona correctamente. </p>
+    </div>
+  );
+}
+
+export default App;
